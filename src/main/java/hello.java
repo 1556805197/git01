8c9a782b552a79cf306d453ff5ceec1fcd03a7b0
1556805197 <1556805197@qq.com>
2018-04-12 19:29:19 +0800

    git03333

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -5,8 +5,8 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hahaha2s");
 
-        System.out.println("h");
-        System.out.println("hahaha2s"); System.out.println("hahaha2s");
+        System.out.println("");
+        System.out.println("hahaa2s"); System.out.println("hahaha2s");
         //
 
 
