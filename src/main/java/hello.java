a67d27a3e3af13745f7438c2edb2fa3ce4a9e5df
1556805197 <1556805197@qq.com>
2018-04-12 17:35:08 +0800

    third

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,7 +2,8 @@
  * Created by John on 2018/4/12.
  */
 public class hello {
-     public static void main(String []args) {
-             System.out.println("hahaha2s");
-         }
+    public static void main(String[] args) {
+        System.out.println("hahaha2s");
+        System.out.println("hahaha2s");
+    }
 }
