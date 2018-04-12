0c97c8c25b51bb9aa032a69eb3add23b44249bcf
1556805197 <1556805197@qq.com>
2018-04-12 19:54:51 +0800

    git033333

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,18 +3,6 @@
  */
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hahaha2s");
-        System.out.println("ha");
-        System.out.println("hdas");
-         //fsd
 
-        System.out.println("h");
-        System.out.println("haa2s");
-        System.out.println("hahaha2s");
-        //g
-        for (int i = 0; i < 10; i++) {
-        }
-
-        System.out.println("hah");
     }
 }
